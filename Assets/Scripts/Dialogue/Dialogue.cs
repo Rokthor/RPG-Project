141de4d9c601b4513e66787253aec43f417d8219
Cody H <80700391+Rokthor@users.noreply.github.com>
2021-04-13 21:23:36 -0700

    ..

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
-public class Dialogue : ScriptableObject
+
+namespace RPG.Dialogue
 {
-    [SerializeField] DialogueNode[] nodes;
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField] DialogueNode[] nodes;
+    }
 }
+
--- Assets/Scripts/Dialogue/DialogueNode.cs
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueNode 
+namespace RPG.Dialogue
 {
-    public string uniqueID;
-    public string text;
-    public string[] children;
+    [System.Serializable]
+    public class DialogueNode
+    {
+        public string uniqueID;
+        public string text;
+        public string[] children;
 
+    }
 }
+
+
